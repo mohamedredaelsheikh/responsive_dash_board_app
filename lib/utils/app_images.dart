cbbf5899c83e0b95722431edcd937a2ec73baf20
mohamedredaelsheikh <moh.reda198@gmail.com>
2025-07-05 16:08:26 +0300

    add assets image

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar_1.svg
+  static const String imagesAvatar1 = "assets/images/avatar_1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar_2.svg
+  static const String imagesAvatar2 = "assets/images/avatar_2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesDash board
+  /// assets/images/dash board.svg
+  static const String imagesDashboard = "assets/images/dash board.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMyCard
+  /// assets/images/my_card.svg
+  static const String imagesMyCard = "assets/images/my_card.svg";
+
+  /// Assets for imagesMyInvestments
+  /// assets/images/my_investments.svg
+  static const String imagesMyInvestments = "assets/images/my_investments.svg";
+
+  /// Assets for imagesSettingIcon
+  /// assets/images/setting-icon.svg
+  static const String imagesSettingIcon = "assets/images/setting-icon.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesWalletAccount
+  /// assets/images/wallet_account.svg
+  static const String imagesWalletAccount = "assets/images/wallet_account.svg";
+}
