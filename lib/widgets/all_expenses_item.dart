b832c5df3c5f2c0a9c8d75fe0b41f775439a1647
Mohamed Elsheikh <93085489+mohamedredaelsheikh@users.noreply.github.com>
2025-07-26 21:23:00 +0300

    add all expenses item

---
+++ lib/widgets/all_expenses_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board_app/models/all_expenses_item_model.dart';
+import 'package:responsive_dash_board_app/utils/app_styles.dart';
+import 'package:responsive_dash_board_app/widgets/all_expenses_item_header.dart';
+
+class AllExpensesItem  extends StatelessWidget{
+  const AllExpensesItem({super.key, required this.allExpensesItemModel});
+  final AllExpensesItemModel allExpensesItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+   return Expanded(
+     child: Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(width: 1, color: const Color(0xFFF1F1F1)),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        color: Colors.white),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AllExpensesItemHeader(imageIcon: allExpensesItemModel.image ,),
+          SizedBox(height: 34,),
+          Text(allExpensesItemModel.title,
+           style: AppStyles.styleSemiBold16(context),
+           ),
+           SizedBox(height: 8,),
+          Text(allExpensesItemModel.date,
+           style: AppStyles.styleRegular14(context),),
+           SizedBox(height: 16,),
+            Text(allExpensesItemModel.amount,
+             style: AppStyles.styleSemiBold24(context),),
+     
+     
+        ],),
+     
+     
+     ),
+   );
+  }
+} 
