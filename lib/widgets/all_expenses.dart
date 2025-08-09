036205459a64cb224272983c0b13fa4d44433f7e
Mohamed Elsheikh <m.reda@cresignzone.com>
2025-08-09 18:36:57 +0300

    create custom back ground container

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board_app/widgets/all_expenses_header.dart';
 import 'package:responsive_dash_board_app/widgets/all_expenses_items_list_view.dart';
+import 'package:responsive_dash_board_app/widgets/custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      ),
+    return CustomBackGroundContainer(
       child: Column(
         children: [
           AllExpensesHeader(),
           SizedBox(height: 16),
           AllExpensesItemsListView(),
-              ],
-            ) ,
-        
-
-      
+        ],
+      ),
     );
   }
 }
